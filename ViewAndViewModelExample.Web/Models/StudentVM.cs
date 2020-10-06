1017871a3e58e6b2505ffdbaa697d64d66578dae
Chayenne <chayenne.vandecasteele.1803@hotmail.be>
2020-10-06 15:36:01 +0100

    Added Model StudentVM

---
+++ ViewAndViewModelExample.Web/Models/StudentVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wba.Oefening.Students.Domain;
+
+namespace ViewAndViewModelExample.Web.Models
+{
+    public class StudentVM
+    {
+        public Course course { get; set; }
+        public Student student { get; set; }
+        public IEnumerable<Course> courses { get; set; }
+        public IEnumerable<Student> students { get; set; }
+    }
+}
---
+++ Wba.Oefening.Students.Domain/CourseRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wba.Oefening.Students.Domain
+{
+   public class CourseRepository
+    {
+        public IEnumerable<Course> GetCourses()
+        {
+            return new List<Course>
+            {
+                new Course{ Id = 0, Name = "GameDeveloper"},
+                new Course{ Id = 1, Name = "AppDeveloper"}
+            };
+        }
+    }
+}
