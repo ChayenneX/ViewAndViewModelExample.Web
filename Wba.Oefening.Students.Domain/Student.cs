83338e1e069110bf171dad76a1a71976501d577f
Chayenne <chayenne.vandecasteele.1803@hotmail.be>
2020-10-06 15:28:00 +0100

    Added classes to library

---
+++ Wba.Oefening.Students.Domain/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wba.Oefening.Students.Domain
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Course { get; set; }
+    }
+}
---
+++ Wba.Oefening.Students.Domain/StudentRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wba.Oefening.Students.Domain
+{
+   public class StudentRepository
+    {
+        public IEnumerable<Student> GetStudents()
+        {
+            return new List<Student>
+            {
+                new Student{ Id = 0, Lastname = "Debouwer", Firstname = "Bob", Course = "Bouwvakker"},
+                new Student{ Id = 1, Lastname = "Frey", Firstname = "Irma", Course = "Kuisvrouw"},
+                new Student{ Id = 2, Lastname = "Deman", Firstname = "Evert", Course = "Accountant"}
+            };
+        }
+    }
+}
